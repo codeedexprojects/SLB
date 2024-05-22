371ac4d3b54fd70fe9697af315972a05e581c9d7
codeedexbackend <prithew.codeedex@gmail.com>
2024-05-22 15:19:55 +0530

    settings

--- SLB/settings.py
+++ SLB/settings.py
@@ -143,9 +143,12 @@ MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 
 # Additional locations of static files
+
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    BASE_DIR / "static",
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
